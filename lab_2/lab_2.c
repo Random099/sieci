d46124359253ae8beb68b00b732cda9c94e81014
Random <60650489+Random099@users.noreply.github.com>
2023-11-22 09:28:17 +0100

    Update lab_2.c

--- lab_2/lab_2.c
+++ lab_2/lab_2.c
@@ -1,15 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
 
 #define ARRAYSIZE(a) (sizeof(a)/sizeof(a[0]))
 
 struct Packet{
-    int id;
+    uint32_t id;
+    uint32_t byteCount;
     unsigned char data[3];
 };
 
+unsigned char* strToDecStr(unsigned char* string, unsigned char* dst){
+    for(int i = 0; i < strlen(string); i++){
+        unsigned char* temp = malloc(sizeof(unsigned char)*2);
+        sprintf(temp, "%d", string[i]);
+        strcat(dst, temp);
+        strcat(dst, " ");
+        free(temp);
+    }
+    return dst;
+}
+
 int main(){
-    FILE* fp = fopen("test.txt", "rb");
+    FILE* dataFile = fopen("test.bin", "wb");
+    if (dataFile != NULL){
+        unsigned char data[] = {0xFF, 0xFF, 0xFF};
+        fwrite(data, sizeof(data), 1, dataFile);
+        fclose(dataFile);
+    }
+    FILE* fp = fopen("test.bin", "rb");
     struct Packet readPacket;
     
     //Calculate file size and return to beginning
@@ -22,8 +42,10 @@ int main(){
 
     //Read file into buffer of desired form packets
     int bufferIdx = 0;
-    for(; (fread(&readPacket.data, sizeof(readPacket.data) - 1, 1, fp) == 1); ++bufferIdx){
-        readPacket.id = bufferIdx;
+    size_t chunkSize = sizeof(readPacket.data) - 1;
+    for(; (fread(&readPacket.data, chunkSize, 1, fp) == 1); ++bufferIdx){
+        readPacket.id = bufferIdx+1;
+        readPacket.byteCount = chunkSize;
         buffer[bufferIdx] = readPacket; 
         buffer[bufferIdx].data[ARRAYSIZE(readPacket.data) - 1] = 0x00;
     }
@@ -32,14 +54,21 @@ int main(){
     int bytesLeft = fileSize - (bufferIdx * 2);
     if(bytesLeft != 0){
         fread(&readPacket.data, bytesLeft, 1, fp);
-        readPacket.id = bufferIdx;
+        readPacket.id = bufferIdx+1;
+        readPacket.byteCount = bytesLeft;
         readPacket.data[bytesLeft] = 0x00;
         buffer[bufferIdx] = readPacket;
         bufferIdx++; 
     }
 
-    for (int i = 0; i < bufferIdx; i++)
-        printf("%d, %s\n", buffer[i].id, buffer[i].data);
+    unsigned char* printPtr = malloc(sizeof(readPacket.data));
+    printPtr[0] = '\0';
+    for (int i = 0; i < bufferIdx; i++){
+        printf("id: %08d byte count: %08d data: %s\n", buffer[i].id, buffer[i].byteCount, strToDecStr(buffer[i].data, printPtr));
+        memset(printPtr, 0, strlen(printPtr));
+    }
+
+    free(printPtr);
     free(buffer);
     fclose(fp);
     return 0;
